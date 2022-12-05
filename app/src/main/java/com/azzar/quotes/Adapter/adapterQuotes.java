85a07b047c7788d1a338f383f74164b11b982bc6
AzzarRizky <azzar.arza18@gmail.com>
2022-12-05 10:28:56 +0700

    update AdapterQuotes

--- app/src/main/java/com/azzar/quotes/Adapter/adapterQuotes.java
+++ app/src/main/java/com/azzar/quotes/Adapter/adapterQuotes.java
@@ -1,5 +1,60 @@
 package com.azzar.quotes.Adapter;
 
-public class adapterQuotes {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.azzar.quotes.Model.QuotesModel;
+import com.azzar.quotes.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class adapterQuotes extends RecyclerView.Adapter<adapterQuotes.ViewHolderQuotes>{
+
+    private List<QuotesModel> listQuotes = new ArrayList<>();
+    private Context ctx;
+
+    public adapterQuotes(List<QuotesModel> listQuotes, Context ctx) {
+        this.listQuotes = listQuotes;
+        this.ctx = ctx;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderQuotes onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View varView = LayoutInflater.from(ctx).inflate(R.layout.item_quotes,parent,false);
+
+    return new ViewHolderQuotes(varView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderQuotes holder, int position) {
+        QuotesModel QM = listQuotes.get(position);
+        holder.tvQuotes.setText(QM.getText());
+        holder.tvAuthor.setText(QM.getAuthor());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listQuotes.size();
+    }
+
+    public class ViewHolderQuotes extends RecyclerView.ViewHolder{
+        TextView tvQuotes,tvAuthor;
+
+        public ViewHolderQuotes(@NonNull View itemView) {
+            super(itemView);
+            tvQuotes = itemView.findViewById(R.id.tv_quotes);
+            tvAuthor = itemView.findViewById(R.id.tv_author);
+
+        }
+    }
 }
